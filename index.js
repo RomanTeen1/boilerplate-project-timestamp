7dbf5e4023bbbfcf87024da2d74e60c484da259d
RomanTeen1 <fquez716@gmail.com>
2023-03-31 19:25:06 -0700

    this is my completed project for boilerplate-project-template
    My Project completion

--- index.js
+++ index.js
@@ -20,13 +20,33 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api", function (req, res) {
+  res.json({unix: new Date().getTime(), utc: new Date().toUTCString()});
 });
 
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
 
+// API for 1st link 2015-12-25
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date);
+  // console.log(date);
+  // console.log(Date.parse(date));
+
+  // If the date is an invalid date, change it to where it is a valid date!
+  if(isInvalidDate(date))
+  {
+    date = new Date(Number(req.params.date));
+  }
+
+  if(isInvalidDate(date)){
+    res.json({error: "Invalid date"});
+    return;
+  }
+  
+  res.json({unix: date.getTime(), utc: date.toUTCString()});
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
